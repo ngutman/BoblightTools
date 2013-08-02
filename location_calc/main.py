b224dd52e8083d20b0c675364fb4277d40d88507
ngutman <nimrod@billguard.com>
2013-08-02 19:57:27 +0300

    move files

--- location_calc/main.py
+++ location_calc/main.py
@@ -16,8 +16,8 @@ def main():
         hpos_per = (int(light[0]) + int(light[1])) / 2.0
         vpos_per = (int(light[2]) + int(light[3])) / 2.0
 
-        hpos = H * (hpos_per / 100.0)
-        vpos = V - V * (vpos_per / 100.0)
+        hpos = H - H * (hpos_per / 100.0)
+        vpos = V * (vpos_per / 100.0)
 
         print "LED #" + str(i + 1).ljust(3),
         print "|\t" + str(hpos) + "\t|\t" + str(vpos)
